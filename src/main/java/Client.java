610c6af652168e93d3bf965cb39ffd35d36548a8
Avi Mizrahi <avi1mizrahi@gmail.com>
2018-12-20 19:43:05 +0200

    Client I/F, basic implementation

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,35 +1,89 @@
-import ClientServerCommunication.ClientGrpc;
-import ClientServerCommunication.CreateAccountReq;
-import ClientServerCommunication.CreateAccountRsp;
-import io.grpc.ManagedChannel;
+import ClientServerCommunication.*;
 import io.grpc.ManagedChannelBuilder;
-import io.grpc.StatusRuntimeException;
+
+import java.util.Optional;
+import java.util.OptionalInt;
+
 
 public class Client {
+
+    private static ClientGrpc.ClientBlockingStub stub;
+
+    Client(String name, int port) {
+        stub = ClientGrpc.newBlockingStub(ManagedChannelBuilder.forAddress(name, port)
+                                                               .usePlaintext()
+                                                               .build());
+    }
+
     public static void main(String[] args) {
-        ClientGrpc.ClientBlockingStub stub;
 
-        try {
-            ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost",
-                                                                      50505)
-                                                          .usePlaintext()
-                                                          .build();
-            stub = ClientGrpc.newBlockingStub(channel);
-        } catch (StatusRuntimeException e) {
-            e.printStackTrace();
-            return;
+        // TODO: create a test class for client-server communication
+        // TODO: migrate this test to there and remove this main
+        // TODO: create a mock-server (we can use "mockito" for mocking automation)
+        // TODO: add tests for basic scenarios
+        // TODO: optional (I'm not sure if it's cost-effective):
+        //       - write here a main-loop that will receive commands interactively
+        // TODO: somehow make the test run once with a mock-server (aka unittest)
+        //        and once with the real server which we haven't yet (aka integration test)
+
+        var client    = new Client("localhost", 55555);
+        var account1 = client.createAccount().get();
+        var account2 = client.createAccount().get();
+
+        client.addAmount(account1, 50);
+        client.transfer(account1, account2, 50);
+        assert client.getAmount(account1).getAsInt() == 0;
+        assert client.getAmount(account2).getAsInt() == 50;
+
+        client.deleteAccount(account1);
+        client.deleteAccount(account2);
+    }
+
+    Optional<Account> createAccount() {
+        var response = stub.createAccount(CreateAccountReq.getDefaultInstance());
+
+        if (!response.getSuccess()) {
+            return Optional.empty();
         }
+        return Optional.of(new Account(response.getId()));
+    }
 
-        CreateAccountReq request = CreateAccountReq.newBuilder().build();
-        CreateAccountRsp response;
+    void deleteAccount(Account account) {
+        stub.deleteAccount(DeleteAccountReq.newBuilder().setAccountId(account.getId()).build());
+    }
 
-        try {
-            response = stub.createAccount(request);
-        } catch (StatusRuntimeException e) {
-            e.printStackTrace();
-            return;
+    boolean addAmount(Account account, int amount) {
+        var response = stub.addAmount(AddAmountReq.newBuilder()
+                                                  .setAccountId(account.getId())
+                                                  .setAmount(amount)
+                                                  .build());
+        return response.getSuccess();
+    }
+
+    OptionalInt getAmount(Account account) {
+        var response = stub.getAmount(GetAmountReq.newBuilder()
+                                                  .setAccountId(account.getId())
+                                                  .build());
+        if (!response.getSuccess()) {
+            return OptionalInt.empty();
         }
+        return OptionalInt.of(response.getAmount());
+    }
 
-        System.out.println("CLIENT: " + response);
+    boolean transfer(Account from, Account to, int amount) {
+        var response = stub.transfer(TransactionReq.newBuilder()
+                                                   .setFromId(from.getId())
+                                                   .setToId(to.getId())
+                                                   .setAmount(amount)
+                                                   .build());
+        return response.getSuccess();
+    }
+
+    static class Account {
+        private final int id;
+
+        Account(int id) {this.id = id;}
+
+        public int getId() {return id;}
     }
 }
