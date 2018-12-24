54fa66c10c69bda58c12af103dd226962286f3c3
Avi Mizrahi <avi1mizrahi@gmail.com>
2018-12-24 10:36:17 +0200

    add async test

--- src/test/java/IntegrationTest.java
+++ src/test/java/IntegrationTest.java
@@ -1,6 +1,9 @@
+import io.grpc.netty.shaded.io.netty.util.internal.ConcurrentSet;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -54,6 +57,91 @@ class IntegrationTest {
         client1.deleteAccount(account3);
         client2.deleteAccount(account4);
 
+        client1.shutdown();
+        client2.shutdown();
         server[0].shutdown();
+    }
+
+    @Test
+    void async_independent() {
+        final Server[] server = new Server[1];
+        assertDoesNotThrow(() -> {
+            server[0] = new ServerBuilder().setPort(PORT)
+                                           .setBlockWindow(Duration.ofMillis(100)) // TODO: 100 is just to accelerate the tests, don't know what is "good value"
+                                           .createServer()
+                                           .start();
+        });
+
+        var threads = new ArrayList<Thread>();
+        var clients = new ConcurrentSet<Client>();
+
+        for (int i = 0; i < 100; ++i) {
+            threads.add(new Thread(() -> {
+                var client1  = new Client(LOCALHOST, PORT);
+                var client2  = new Client(LOCALHOST, PORT);
+
+                clients.add(client1);
+                clients.add(client2);
+
+                var account1 = client1.createAccount().get();
+                addSchedulingNoise();
+                var account2 = client1.createAccount().get();
+                var account3 = client2.createAccount().get();
+                var account4 = client2.createAccount().get();
+
+                client1.addAmount(account1, 100);
+                client1.addAmount(account3, 100);
+
+                client2.transfer(account1, account2, 50);
+                client2.transfer(account1, account3, 50);
+
+                assertEquals(client1.getAmount(account1).getAsInt(), 0);
+                assertEquals(client2.getAmount(account2).getAsInt(), 50);
+                assertEquals(client1.getAmount(account3).getAsInt(), 150);
+                assertEquals(client2.getAmount(account4).getAsInt(), 0);
+
+                client1.transfer(account3, account1, 150);
+
+                assertEquals(client2.getAmount(account1).getAsInt(), 150);
+                assertEquals(client1.getAmount(account3).getAsInt(), 0);
+
+                int value1 = 150;
+
+                for (int j = 0; j < 30; ++j) {
+                    client1.addAmount(account1, 20);
+                    client2.addAmount(account1, 20);
+                    value1 += 40;
+                    assertEquals(client1.getAmount(account1).getAsInt(), value1);
+                }
+
+                client1.deleteAccount(account1);
+                client2.deleteAccount(account2);
+                client1.deleteAccount(account3);
+                client2.deleteAccount(account4);
+            }));
+        }
+
+        threads.forEach(Thread::start);
+        threads.forEach(thread -> {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                assert false;
+            }
+        });
+
+        clients.forEach(Client::shutdown);
+
+        server[0].shutdown();
+    }
+
+    private static void addSchedulingNoise() {
+        final int delay = ThreadLocalRandom.current().nextInt(1000);
+        if (delay < 200) return;
+        try {
+            Thread.sleep(delay);
+        } catch (InterruptedException ignored) {
+        }
     }
 }
