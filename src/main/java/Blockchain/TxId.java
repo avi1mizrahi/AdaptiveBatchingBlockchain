983f650a6a005e2046eac83ad54569f17eb5f816
Avi Mizrahi <avi1mizrahi@gmail.com>
2019-01-08 22:00:10 +0200

    TxId: change separator, override object methods

--- src/main/java/Blockchain/TxId.java
+++ src/main/java/Blockchain/TxId.java
@@ -3,8 +3,12 @@ package Blockchain;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
+import java.util.Objects;
+
 @Data
 public class TxId {
+    public static final String SEPARATOR = "-";
+
     private final int serverId;
     private final int blockIdx;
     private final int txIdx;
@@ -18,30 +22,34 @@ public class TxId {
     }
 
     public static TxId from(String stringId) {
-        String[] split = stringId.split(":");
+        String[] split = stringId.split(SEPARATOR);
         if (split.length != 3) throw new RuntimeException(stringId + " is not a valid TxId");
         return new TxId(Integer.valueOf(split[0]),
                         Integer.valueOf(split[1]),
                         Integer.valueOf(split[2]));
     }
 
+    public boolean equals(TxId other) {
+        return this.serverId == other.serverId &&
+                this.blockIdx == other.blockIdx &&
+                this.txIdx == other.txIdx;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return obj instanceof TxId && equals((TxId) obj);
+    }
+
     @Override
     public String toString() {
-        return String.join(":",
+        return String.join(SEPARATOR,
                            String.valueOf(serverId),
                            String.valueOf(blockIdx),
                            String.valueOf(txIdx));
     }
 
-    public int getServerId() {
-        return serverId;
-    }
-
-    public int getBlockIdx() {
-        return blockIdx;
-    }
-
-    public int getTxIdx() {
-        return txIdx;
+    @Override
+    public int hashCode() {
+        return Objects.hash(serverId, blockIdx, txIdx);
     }
 }
